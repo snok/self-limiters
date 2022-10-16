e24c3d073dae3de638738f427f9f2777e64c1cdf
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-10-16 16:12:19 +0200

    refactor: Replace Semaphore creation with pipelined commands
    It's faster!

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -134,42 +134,14 @@ async fn release_semaphore(receiver: Receiver<ThreadState>) -> SLResult<()> {
     // Connect to redis
     let mut connection = ts.client.get_async_connection().await?;
 
-    // Define queue if it doesn't exist
-    Script::new(
-        r"
-        --- Script called from the Semaphore implementation.
-        ---
-        --- Lua scripts are run atomically by default, and since redis
-        --- is single threaded, there are no race conditions to worry about.
-        ---
-        --- The script releases the semaphore (pushes back the popped entry
-        --- to our list) and sets and expiry on all related keys.
-        ---
-        --- keys:
-        --- * key: The key to use for the list
-        ---
-        --- returns:
-        --- * Nothing
-
-        redis.replicate_commands()
-
-        -- Init config variables
-        local key = tostring(KEYS[1])
-
-        -- Add back capacity to the queue
-        redis.call('LPUSH', key, 1)
-
-        -- Then set expiry for the queue
-        redis.call('EXPIRE', key, 30)
-
-        -- Then set expiry for the key we use to check if the queue exists
-        -- See comments in the other semaphore script for a detailed explanation
-        -- of this value.
-        redis.call('EXPIRE', string.format('(%s)-exists', key), 30)",
-    )
-    .key(&ts.name)
-    .invoke_async(&mut connection)
-    .await?;
+    // Push capacity back to the semaphore
+    // *We don't care about this being atomic
+    redis::pipe()
+        .lpush(&ts.name, 1)
+        .expire(&ts.name, 30)
+        .expire(format!("{}-exists", &ts.name), 30)
+        .query_async(&mut connection)
+        .await?;
 
     debug!("Released semaphore");
     Ok(())
